cf94d8a536b1f267e4a51e48e354693d77f3e4fa
RJoshima <sojoy82@yahoo.co.jp>
2018-05-06 05:46:05 +0900

    fix: contract

--- contracts/CreateFighter.sol
+++ contracts/CreateFighter.sol
@@ -1,8 +1,86 @@
 pragma solidity ^0.4.21;
-import "./safemath.sol";
-import "./ownable.sol";
 
 // import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
 
 contract CreateFighter is Ownable {
 
@@ -16,6 +94,8 @@ contract CreateFighter is Ownable {
     uint16 winCount;
     uint16 lossCount;
   }
+
+
 
   mapping (uint => address) public fighterToOwner;
   mapping (address => uint) ownerFighterCount;
@@ -109,3 +189,32 @@ contract CreateFighter is Ownable {
   }
 
 }
+
+
+
+
+    // function tokensOfOwner(address _owner) external view returns(uint256[] ownerTokens) {
+    //     uint256 tokenCount = balanceOf(_owner);
+
+    //     if (tokenCount == 0) {
+    //         // Return an empty array
+    //         return new uint256[](0);
+    //     } else {
+    //         uint256[] memory result = new uint256[](tokenCount);
+    //         uint256 totalCats = totalSupply();
+    //         uint256 resultIndex = 0;
+
+    //         // We count on the fact that all cats have IDs starting at 1 and increasing
+    //         // sequentially up to the totalCat count.
+    //         uint256 catId;
+
+    //         for (catId = 1; catId <= totalCats; catId++) {
+    //             if (kittyIndexToOwner[catId] == _owner) {
+    //                 result[resultIndex] = catId;
+    //                 resultIndex++;
+    //             }
+    //         }
+
+    //         return result;
+    //     }
+    // }
